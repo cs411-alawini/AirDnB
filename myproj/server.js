6b4bf6a8db85aebc0b9568fa8bf2a36bb57d9a7c
Achintya Sanjay <55640537+asanjay123@users.noreply.github.com>
2024-04-28 21:31:38 -0500

    Third Advanced Query

--- myproj/server.js
+++ myproj/server.js
@@ -44,38 +44,34 @@ app.get('/api/restaurants', function(req, res) {
   });
 });
 
-app.get('/api/boroughs/restaurants', function(req, res) {
+app.get('/airbnbsByBorough', function(req, res) {
   var sql = `
-    SELECT
-        b.Borough,
-        COUNT(r.RestaurantID) AS NumberOfRestaurants
-    FROM (
-        SELECT 'Queens' AS Borough, 40.5417 AS MinLat, 40.8007 AS MaxLat, -73.9626 AS MinLong, -73.7004 AS MaxLong
-        UNION ALL
-        SELECT 'Bronx', 40.7855, 40.9176, -73.9332, -73.7654
-        UNION ALL
-        SELECT 'Staten Island', 40.4774, 40.6521, -74.2591, -74.0345
-        UNION ALL
-        SELECT 'Manhattan', 40.7002, 40.8767, -74.0182, -73.9106
-        UNION ALL
-        SELECT 'Brooklyn', 40.5707, 40.7395, -74.0567, -73.8334
-    ) AS b
-    LEFT JOIN
-        Restaurants AS r ON (
-            r.Latitude BETWEEN b.MinLat AND b.MaxLat
-            AND r.Longitude BETWEEN b.MinLong AND b.MaxLong
-        )
-    GROUP BY
-        b.Borough;
+  SELECT
+      b.Borough,
+      COUNT(l.ListingID) AS NumberOfAirbnbListings
+  FROM
+      (
+          SELECT 'Bronx' AS Borough, 40.8082 AS MinLat, 40.9093 AS MaxLat, -73.911 AS MinLong, -73.804 AS MaxLong
+          UNION ALL
+          SELECT 'Queens', 40.5915, 40.771, -73.956, -73.76286
+          UNION ALL
+          SELECT 'Staten Island', 40.5083, 40.6393, -74.2010, -74.0562
+          UNION ALL
+          SELECT 'Manhattan', 40.7059, 40.8164, -73.978, -73.928
+          UNION ALL
+          SELECT 'Brooklyn', 40.5876, 40.7277, -74.007, -73.8776
+      ) AS b
+  LEFT JOIN
+      AirBnBListing AS l ON (
+          l.Latitude BETWEEN b.MinLat AND b.MaxLat
+          AND l.Longitude BETWEEN b.MinLong AND b.MaxLong
+      )
+  GROUP BY
+      b.Borough
   `;
-
-  connection.query(sql, function(err, results) {
-    if (err) {
-      console.error('Error fetching restaurant count by borough:', err);
-      res.status(500).send({ message: 'Error fetching restaurant count by borough', error: err });
-      return;
-    }
-    res.json(results);
+  connection.query(sql, function (error, results, fields) {
+      if (error) throw error;
+      res.send(results);
   });
 });
 
