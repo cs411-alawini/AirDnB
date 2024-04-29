ffc399452b1029d874b217fd0798dee3269a6530
Achintya Sanjay <55640537+asanjay123@users.noreply.github.com>
2024-04-28 20:04:13 -0500

    Update with new advanced queries

--- myproj/server.js
+++ myproj/server.js
@@ -78,7 +78,67 @@ app.get('/api/boroughs/restaurants', function(req, res) {
   });
 });
 
+app.get('/subwayStationsByBorough', function(req, res) {
+  var sql = `
+  SELECT
+      b.Borough,
+      COUNT(s.StationName) AS NumberOfSubwayStations
+  FROM
+      (
+          SELECT 'Bronx' AS Borough, 40.8082 AS MinLat, 40.9093 AS MaxLat, -73.911 AS MinLong, -73.804 AS MaxLong
+          UNION ALL
+          SELECT 'Queens', 40.5915, 40.771, -73.956, -73.76286
+          UNION ALL
+          SELECT 'Staten Island', 40.5083, 40.6393, -74.2010, -74.0562
+          UNION ALL
+          SELECT 'Manhattan', 40.7059, 40.8164, -73.978, -73.928
+          UNION ALL
+          SELECT 'Brooklyn', 40.5876, 40.7277, -74.007, -73.8776
+      ) AS b
+  LEFT JOIN
+      SubwayStation AS s ON (
+          s.Latitude BETWEEN b.MinLat AND b.MaxLat
+          AND s.Longitude BETWEEN b.MinLong AND b.MaxLong
+      )
+  GROUP BY
+      b.Borough;
+  `;
+  connection.query(sql, function (error, results, fields) {
+      if (error) throw error;
+      res.send(results);
+  });
+});
 
+app.get('/restaurantsByBorough', function(req, res) {
+  var sql = `
+  SELECT
+      b.Borough,
+      COUNT(r.RestaurantID) AS NumberOfRestaurants
+  FROM
+      (
+          SELECT 'Bronx' AS Borough, 40.8082 AS MinLat, 40.9093 AS MaxLat, -73.911 AS MinLong, -73.804 AS MaxLong
+          UNION ALL
+          SELECT 'Queens', 40.5915, 40.771, -73.956, -73.76286
+          UNION ALL
+          SELECT 'Staten Island', 40.5083, 40.6393, -74.2010, -74.0562
+          UNION ALL
+          SELECT 'Manhattan', 40.7059, 40.8164, -73.978, -73.928
+          UNION ALL
+          SELECT 'Brooklyn', 40.5876, 40.7277, -74.007, -73.8776
+      ) AS b
+  LEFT JOIN
+      Restaurants AS r ON (
+          r.Latitude BETWEEN b.MinLat AND b.MaxLat
+          AND r.Longitude BETWEEN b.MinLong AND b.MaxLong
+      )
+  GROUP BY
+      b.Borough;
+  `;
+  connection.query(sql, function (error, results, fields) {
+      if (error) throw error;
+      res.send(results);
+  });
+});
 
 app.listen(80, function () {
     console.log('Node app is running on port 80');
