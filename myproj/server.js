019ae0e648a9f89e53440d8655273687dd98ea16
Achintya Sanjay <55640537+asanjay123@users.noreply.github.com>
2024-04-30 00:23:47 -0500

    ListingID Page

--- myproj/server.js
+++ myproj/server.js
@@ -212,27 +212,27 @@ async function getCrimeDataNearby(listingID) {
   return db.query(query, [listingID]);
 }
 
-app.get('/results', async (req, res) => {
-  const listingID = req.query.listingID;
-  if (!listingID) {
-      return res.status(400).send('Listing ID is required');
-  }
+// app.get('/results', async (req, res) => {
+//   const listingID = req.query.listingID;
+//   if (!listingID) {
+//       return res.status(400).send('Listing ID is required');
+//   }
 
-  try {
-      const restaurantData = await getClosestRestaurant(listingID);
-      const subwayData = await getClosestSubwayStation(listingID);
-      const crimeData = await getCrimeDataNearby(listingID);
-      res.json({ restaurantData, subwayData, crimeData });
-  } catch (error) {
-      res.status(500).send('Server error');
-  }
-});
+//   try {
+//       const restaurantData = await getClosestRestaurant(listingID);
+//       const subwayData = await getClosestSubwayStation(listingID);
+//       const crimeData = await getCrimeDataNearby(listingID);
+//       res.json({ restaurantData, subwayData, crimeData });
+//   } catch (error) {
+//       res.status(500).send('Server error');
+//   }
+// });
 
-function extractRoomId(url) {
-  const regex = /rooms\/(\d+)/;
-  const match = url.match(regex);
-  return match ? match[1] : null;
-}
+// function extractRoomId(url) {
+//   const regex = /rooms\/(\d+)/;
+//   const match = url.match(regex);
+//   return match ? match[1] : null;
+// }
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/parse-url', function(req, res) {
